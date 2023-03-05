6f721bc98a46ad665146d12f6ffee3a28aeb3dc9
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-05 19:42:56 +0100

    ejr7

---
+++ ejercicio7.py
@@ -0,0 +1,5 @@
+#Escribir un algoritmo iterativo que transforme un número entero n cualquiera en su representación en una base B ≥ 2 cualquiera.
+print("Elija un numero y la base donde quiere su representacion siendo esta mayor que 2")
+n = int(input("Numero: ")) #permitimos introducir el numero a cambiar
+B = int(input("Base: ")) #permitimos introducir la base 
+
